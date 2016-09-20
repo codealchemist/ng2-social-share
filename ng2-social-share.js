ab98c6bc5280f4b7e02ac6d92aa0d9cb1c2ae00f
Alberto Miranda <codealchemist@gmail.com>
2016-09-20 10:49:09 -0300

    added missing files

--- ng2-social-share.js
+++ ng2-social-share.js
@@ -131,15 +131,15 @@ var CeiboShare = (function () {
                 str += keys[i] + '=' + encodeURIComponent(p[keys[i]]);
             }
         }
-        var url = sharer.shareUrl + str;
+        sharer.shareUrl += str;
         if (!sharer.isLink) {
-            var popWidth = sharer.width || 600, popHeight = sharer.height || 480, left = window.innerWidth / 2 - popWidth / 2 + window.screenX, top = window.innerHeight / 2 - popHeight / 2 + window.screenY, popParams = 'scrollbars=no, width=' + popWidth + ', height=' + popHeight + ', top=' + top + ', left=' + left, newWindow = window.open(url, '', popParams);
+            var popWidth = sharer.width || 600, popHeight = sharer.height || 480, left = window.innerWidth / 2 - popWidth / 2 + window.screenX, top = window.innerHeight / 2 - popHeight / 2 + window.screenY, popParams = 'scrollbars=no, width=' + popWidth + ', height=' + popHeight + ', top=' + top + ', left=' + left, newWindow = window.open(sharer.shareUrl, '', popParams);
             if (window.focus) {
                 newWindow.focus();
             }
         }
         else {
-            window.location.href = url;
+            window.location.href = sharer.shareUrl;
         }
     };
     CeiboShare.prototype.getSharer = function () {
